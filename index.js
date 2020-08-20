56d39584d0308a11b4198c02c3e250398e4634b5
Freda Hon <freda.hon@gmail.com>
2020-08-20 21:58:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var instruments= ['guitar', 'piano', 'drums', 'bass'];
 
 function theBeatlesPlay(musicians, instruments){
   var array= [ ];
-  for (i = 0; i < musicians.length; i++) {
+  for (var i = 0; i < musicians.length; i++) {
     array.push(musicians[i] + "plays " + instruments[i]);
   }
   return array;
