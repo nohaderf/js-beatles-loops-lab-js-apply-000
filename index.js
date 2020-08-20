0aeb66c08da4959574b3aabfadcb654620a6bf2f
Freda Hon <freda.hon@gmail.com>
2020-08-20 22:35:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,14 @@ function theBeatlesPlay(musicians, instruments){
   return array;
 }
 
-function johnLennonFacts(){
-  var facts = [
-    "He was the last Beatle to learn to drive",
-    "He was never a vegetarian",
-    "He was a choir boy and boy scout",
-    "He hated the sound of his own voice"];
+
+var facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"];
+  
+function johnLennonFacts(facts){
+
   while(facts)
 }
