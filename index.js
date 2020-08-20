fdc85b699aefbc7ab73c34abcfa26f790598b4a2
Freda Hon <freda.hon@gmail.com>
2020-08-20 21:47:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(){
+  var muscicians = [John, Paul, Ringo, George];
+  var instruments = [Guitar, Piano, Drums, Bass];
+  for (i = 0; i < musicians.length; i++) {
+  console.log('');
+  }
+}
+
