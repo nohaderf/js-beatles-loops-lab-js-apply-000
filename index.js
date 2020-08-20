c79bbdd3f483de33e310a0c92cc2a880483bd203
Freda Hon <freda.hon@gmail.com>
2020-08-20 22:00:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // add solution here
-var musicians= ['John Lennon', 'Paul McCartney', 'Ringo Starr', 'George Harrison'];
-var instruments= ['guitar', 'piano', 'drums', 'bass'];
+var musicians= ['John Lennon', 'Paul McCartney','George Harrison', 'Ringo Starr'];
+var instruments= ['Guitar', 'Bass Guitar', 'drums', 'bass'];
 
 function theBeatlesPlay(musicians, instruments){
   var array= [ ];
   for (var i = 0; i < musicians.length; i++) {
-    array.push(musicians[i] + "plays " + instruments[i]);
+    array.push(musicians[i] + " plays " + instruments[i]);
   }
   return array;
 }
